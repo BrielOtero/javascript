e0c0e9f04b8692c333975b32c56ed1a8b70347f8
SrMazas <info.otero.gabriel@gmail.com>
2022-04-20 17:52:33 +0200

    20-04-2022-17-52

--- 02-Js/05-RevisionEvents/01-Exercise/script.js
+++ 02-Js/05-RevisionEvents/01-Exercise/script.js
@@ -1,3 +1,11 @@
 window.onload = function () {
-	
+  var elements = document.getElementById("area");
+  elements.addEventListener("change", seeLenght, false);
 };
+
+function seeLenght() {
+  var areaValue = document.getElementById("area");
+  var stringArea = areaValue.nodeValue;
+  
+  alert(stringArea.toString.length)
+}
